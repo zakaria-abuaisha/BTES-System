28c71be84555f32e483fb9fa052b7b26cf71a27f
Sulieman Abu-Aisha <soliman2020710@gmail.com>
2024-11-25 14:14:59 +0200

    Added the Class Test For Discount And Database

---
+++ BTES.Test/clsDiscountTest.cs
@@ -0,0 +1,36 @@
+﻿using BTES.Business_layer.Discounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BTES.Test
+{
+    public class clsDiscountTest
+    {
+        
+        [Theory]
+        [InlineData(2)]
+        public void IsDiscountExist_ShouldReturnTrue(int DiscountID)
+        {
+            // Act
+            var result = ClsDiscount.IsDiscountExist(DiscountID);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        public void Find_ShouldReturnDiscount(int DiscountID)
+        {
+            // Act
+            var result = ClsDiscount.Find(DiscountID);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
