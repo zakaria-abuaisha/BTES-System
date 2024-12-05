1b3064ea5751425de85dac45ccf88480ceb3e8d4
Sulieman-AbuAisha <soliman2020710@gmail.com>
2024-12-05 09:07:47 +0200

    Add the Business and Data-Access Layer for Waiting List

---
+++ BTES/Business-layer/Tickets/clsWaitingList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTES.Data_Access.Tickets;
+
+namespace BTES.Business_layer.Tickets
+{
+
+    public class clsWaitingList
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int WaitingListID { set; get; }
+        public int EventID { set; get; }
+        public int CustomerID { set; get; }
+        public DateTime JoinDate { set; get; }
+        public int PaymentMethod { set; get; }
+        public int AccountID { set; get; }
+        public string Password { set; get; }
+
+
+        public clsWaitingList()
+        {
+            this.WaitingListID = -1;
+            this.EventID = -1;
+            this.CustomerID = -1;
+            this.JoinDate = DateTime.Now;
+            this.PaymentMethod = -1;
+            this.AccountID = -1;
+            this.Password = "";
+
+            Mode = enMode.AddNew;
+        }
+
+        private clsWaitingList(int WaitingListID, int EventID, int CustomerID, DateTime JoinDate, int PaymentMethod, int AccountID, string Password)
+        {
+            this.WaitingListID = WaitingListID;
+            this.EventID = EventID;
+            this.CustomerID = CustomerID;
+            this.JoinDate = JoinDate;
+            this.PaymentMethod = PaymentMethod;
+            this.AccountID = AccountID;
+            this.Password = Password;
+
+            Mode = enMode.Update;
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewclsWaitingList())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateclsWaitingList();
+
+            }
+
+            return false;
+        }
+
+
+        public static clsWaitingList Findby(int WaitingList_ID)
+        {
+            int Event_ID = -1; int Customer_ID = -1; DateTime JoinDate = DateTime.Now; int Payment_Method = -1; int Account_ID = -1; string Password = ""; ;
+            if (clsWaitingListData.FindByID(WaitingList_ID, ref Event_ID, ref Customer_ID, ref JoinDate, ref Payment_Method, ref Account_ID, ref Password))
+
+                return new clsWaitingList(WaitingList_ID, Event_ID, Customer_ID, JoinDate, Payment_Method, Account_ID, Password);
+            else
+                return null;
+        }
+
+        private bool _AddNewclsWaitingList()
+        {
+            //call DataAccess Layer 
+
+            this.WaitingListID = clsWaitingListData.InsertRecord(this.EventID, this.CustomerID, this.JoinDate, this.PaymentMethod, this.AccountID, this.Password);
+
+            return (this.WaitingListID != -1);
+        }
+
+        private bool _UpdateclsWaitingList()
+        {
+            //call DataAccess Layer 
+
+            return clsWaitingListData.UpdateRecord(this.WaitingListID, this.EventID, this.CustomerID, this.JoinDate, this.PaymentMethod, this.AccountID, this.Password);
+        }
+
+        public static bool DeleteRecord(int WaitingList_ID)
+        {
+            return clsWaitingListData.DeleteRecord(WaitingList_ID);
+        }
+    }
+}
---
+++ BTES/Data-Access/Tickets/clsWaitingListData.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTES.Data_Access.Tickets
+{
+    public class clsWaitingListData
+    {
+
+        //this function will return the new contact id if succeeded and -1 if not.
+        public static int InsertRecord(int Event_ID, int Customer_ID, DateTime JoinDate, int Payment_Method, int Account_ID, string Password)
+        {
+
+            int RecordID = -1;
+
+            SqlConnection connection = clsDatabaseManager.GetInstance();
+
+            string query = @"INSERT INTO Waiting_List  (Event_ID, Customer_ID, JoinDate, Payment_Method, Account_ID, Password)
+                             VALUES (@Event_ID, @Customer_ID, @JoinDate, @Payment_Method, @Account_ID, @Password);
+                             SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@Event_ID", Event_ID);
+            command.Parameters.AddWithValue("@Customer_ID", Customer_ID);
+            command.Parameters.AddWithValue("@JoinDate", JoinDate);
+            command.Parameters.AddWithValue("@Payment_Method", Payment_Method);
+            command.Parameters.AddWithValue("@Account_ID", Account_ID);
+            command.Parameters.AddWithValue("@Password", Password);
+
+
+
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    RecordID = insertedID;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+            }
+
+            finally
+            { connection.Close(); }
+
+            return RecordID;
+
+        }
+        public static bool UpdateRecord(int WaitingList_ID, int Event_ID, int Customer_ID, DateTime JoinDate, int Payment_Method, int Account_ID, string Password)
+        {
+            int rowsAffected = 0;
+
+            SqlConnection connection = clsDatabaseManager.GetInstance();
+
+            string query = @"Update Waiting_List
+                              set WaitingList_ID = @WaitingList_ID , 
+                    Event_ID = @Event_ID , 
+                    Customer_ID = @Customer_ID , 
+                    JoinDate = @JoinDate , 
+                    Payment_Method = @Payment_Method , 
+                    Account_ID = @Account_ID , 
+                    Password = @Password 
+                              where WaitingList_ID = @WaitingList_ID";
+
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@WaitingList_ID", WaitingList_ID);
+            command.Parameters.AddWithValue("@Event_ID", Event_ID);
+            command.Parameters.AddWithValue("@Customer_ID", Customer_ID);
+            command.Parameters.AddWithValue("@JoinDate", JoinDate);
+            command.Parameters.AddWithValue("@Payment_Method", Payment_Method);
+            command.Parameters.AddWithValue("@Account_ID", Account_ID);
+            command.Parameters.AddWithValue("@Password", Password);
+
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+
+        }
+        public static bool FindByID(int WaitingList_ID, ref int Event_ID, ref int Customer_ID, ref DateTime JoinDate, ref int Payment_Method, ref int Account_ID, ref string Password)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = clsDatabaseManager.GetInstance();
+
+            string query = @"SELECT * FROM Waiting_List 
+                             WHERE WaitingList_ID = @WaitingList_ID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@WaitingList_ID", WaitingList_ID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    // The record was found
+                    isFound = true;
+
+                    WaitingList_ID = (int)reader["WaitingList_ID"];
+                    Event_ID = (int)reader["Event_ID"];
+                    Customer_ID = (int)reader["Customer_ID"];
+                    JoinDate = (DateTime)reader["JoinDate"];
+                    Payment_Method = (int)reader["Payment_Method"];
+                    Account_ID = (int)reader["Account_ID"];
+                    Password = (string)reader["Password"];
+
+
+                    reader.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+
+        }
+        public static bool DeleteRecord(int WaitingList_ID)
+        {
+
+            int rowsAffected = 0;
+
+            SqlConnection connection = clsDatabaseManager.GetInstance();
+
+            string query = @"Delete Waiting_List 
+                                        where WaitingList_ID = @WaitingList_ID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@WaitingList_ID", WaitingList_ID);
+
+            try
+            {
+                connection.Open();
+
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                // Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+
+                connection.Close();
+
+            }
+
+            return (rowsAffected > 0);
+
+        }
+    }
+}
